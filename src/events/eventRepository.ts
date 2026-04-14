c04f3134657c6a3d68669d99ee3d6ea2e294d774
Ankiennguyen <ankien.nguyen.2005@gmail.com>
2026-04-14 14:51:08 -0400

    feat: add update method to EventRepository interface

--- src/events/eventRepository.ts
+++ src/events/eventRepository.ts
@@ -3,4 +3,5 @@ import { Event } from "./event";
 export interface EventRepository {
   create(event: Event): Promise<Event>;
   findById(id: string): Promise<Event | null>;
+  update(event: Event): Promise<Event>;
 }
