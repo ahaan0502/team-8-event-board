bd74590bf9eaa85d878cfc035b887eb7c9d12165
Ankiennguyen <ankien.nguyen.2005@gmail.com>
2026-04-14 14:50:40 -0400

    feat: add NotFoundError, NotAuthorizedError, InvalidStateError types

--- src/events/errors.ts
+++ src/events/errors.ts
@@ -1,9 +1,21 @@
 export type EventError =
-  | ReturnType<typeof ValidationError>;
+  | ReturnType<typeof ValidationError>
+  | ReturnType<typeof NotFoundError>
+  | ReturnType<typeof NotAuthorizedError>
+  | ReturnType<typeof InvalidStateError>;
 
 export function ValidationError(message: string) {
-  return {
-    type: "ValidationError",
-    message,
-  };
+  return { type: "ValidationError" as const, message };
+}
+
+export function NotFoundError(message: string) {
+  return { type: "NotFoundError" as const, message };
+}
+
+export function NotAuthorizedError(message: string) {
+  return { type: "NotAuthorizedError" as const, message };
+}
+
+export function InvalidStateError(message: string) {
+  return { type: "InvalidStateError" as const, message };
 }
