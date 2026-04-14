ca1680fb38873b9b9bf6c23ca2183b32e1227805
ahaan0502 <ahaan_singh@hotmail.com>
2026-04-14 15:02:10 -0400

    feat: add getEventById to event repository interface

--- src/events/eventRepository.ts
+++ src/events/eventRepository.ts
@@ -2,5 +2,5 @@ import { Event } from "./event";
 
 export interface EventRepository {
   create(event: Event): Promise<Event>;
-  findById(id: string): Promise<Event | null>;
+  getEventById(eventId: string): Promise<Event | null>;
 }
