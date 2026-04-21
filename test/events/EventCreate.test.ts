e171c43dd1261df8b16bca09dc8277e3fa3f09f4
ahaan0502 <ahaan_singh@hotmail.com>
2026-04-21 15:17:27 -0400

    feat: handled authentication and integration test for successful creation

---
+++ test/events/EventCreate.test.ts
@@ -0,0 +1,48 @@
+import request from "supertest";
+import { createComposedApp } from "../../src/composition";
+
+const app = createComposedApp().getExpressApp();
+const agent = request.agent(app);
+
+const validEventData = {
+    title: "Test Event",
+    description: "Test Description",
+    location: "Test Location",
+    category: "Test Category",
+    startTime: new Date(Date.now() + 10000).toISOString(),
+    endTime: new Date(Date.now() + 20000).toISOString(),
+    capacity: 10,
+};
+
+    beforeEach(async () => {
+  const res = await agent
+    .post("/login")
+    .set("Content-Type", "application/x-www-form-urlencoded")
+    .send("email=user@app.test&password=password123")
+    .redirects(1);
+
+  console.log("LOGIN STATUS:", res.status);
+});
+    
+
+describe("eventService", () => {
+    
+    it("creates event with valid input", async () => {
+  const res = await agent
+    .post("/events")
+    .set("Content-Type", "application/x-www-form-urlencoded")
+    .send(
+      new URLSearchParams({
+        title: "Test Event",
+        description: "Test Description",
+        location: "Test Location",
+        category: "Test Category",
+        startTime: new Date(Date.now() + 10000).toISOString(),
+        endTime: new Date(Date.now() + 20000).toISOString(),
+        capacity: "10"
+      }).toString()
+    );
+
+  expect(res.status).toBe(302);
+});
+})
