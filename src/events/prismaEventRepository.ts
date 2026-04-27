b94955295f9d2e3127052942b71482c5d270dc74
ahaan0502 <ahaan_singh@hotmail.com>
2026-04-27 14:00:09 -0400

    feat: implement prisma getEventById

--- src/events/prismaEventRepository.ts
+++ src/events/prismaEventRepository.ts
@@ -83,11 +83,21 @@ export class PrismaEventRepository implements EventRepository {
     return event ? toDomain(event) : null;
   }
 
-  async getEventsByIds(): Promise<Event[]> {
-    throw new Error("Not implemented yet");
+  async getEventsByIds(eventIds: string[]): Promise<Event[]> {
+    const events = await prisma.event.findMany({
+        where: {
+        id: { in: eventIds },
+        },
+    });
+
+    return events.map(toDomain);
   }
 
-  async getEventsByOrganizerId(): Promise<Event[]> {
-    throw new Error("Not implemented yet");
+  async getEventsByOrganizerId(organizerId: string): Promise<Event[]> {
+    const events = await prisma.event.findMany({
+        where: { organizerId },
+    });
+
+    return events.map(toDomain);
   }
 }
